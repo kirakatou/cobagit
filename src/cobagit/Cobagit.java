d3b4313d511c82c2d3eb46a6b8eb14cb0d2fbc0d
Sutandi <sutandii@icloud.com>
2016-07-23 16:09:58 +0700

    second update

--- src/cobagit/Cobagit.java
+++ src/cobagit/Cobagit.java
@@ -1,7 +1,13 @@
 package cobagit;
 
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Cobagit extends JFrame {
 
@@ -16,6 +22,22 @@ public class Cobagit extends JFrame {
         setSize(800, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new GridBagLayout());
+        JLabel lbl = new JLabel("Name :");
+        JTextField tf = new JTextField(15);
+        JButton button = new JButton("Submit");
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(5,5,5,5);
+        add(lbl, gbc);
+        gbc.gridx++;
+        add(tf, gbc);
+        gbc.gridy++;
+        gbc.gridx = 0;
+        gbc.gridwidth = 2;
+        add(button, gbc);
+        
     }
 
     public static void main(String[] args) {
